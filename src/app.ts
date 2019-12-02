9ed80ffb4369930b70f0ba1fb3feb6463b16879d
Dussikus <dussikus@Pascals-MacBook-Pro.local>
2019-12-02 21:45:13 +0100

    added webpack config and primitive types lesson

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,40 @@
-console.log('Hello TypeScript!');
+// *** TSC Copmiler and tsconfig
+// tsc -version
+// tsc --init
+// tsc
+// tsc --outDir dist
+// tsc -watch
+// *** Alternative we can use webpack.config.js
+// And the application as following:
+// npm install
+// npm start
+
+///*** Primitive Types ***
+console.log('-=====PRIMITIVE TYPES=====-');
+///* Numbers *****************
+console.log('--NUMBERS--');
+
+let bookCost: number = 10;
+function calculatePrice(cost: number): number {
+  return cost;
+}
+const cost: number = calculatePrice(20);
+console.log('The price of the book is: ' + cost);
+
+///* String types *****************
+console.log('--STRING TYPES--');
+const username: string = 'charles';
+
+function normalizeUsername(username: string): string {
+  return username.toUpperCase();
+}
+
+console.log('Normalized username: ' + normalizeUsername('username'));
+
+///* Boolean types *****************
+console.log('--BOOLEAN TYPES--');
+function offerDiscount(orders: number): boolean {
+  return orders >= 3;
+}
+console.log('You will get the discount: ');
+console.log(+offerDiscount(5) ? 'yes' : 'no');
