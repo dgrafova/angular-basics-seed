db4d139f77a279c4dd0ee076776af54864ad1293
dgrafova <d.grafova@gmail.com>
2019-12-05 22:49:26 +0100

    added typescript types

--- src/app.ts
+++ src/app.ts
@@ -38,3 +38,51 @@ function offerDiscount(orders: number): boolean {
 }
 console.log('You will get the discount: ');
 console.log(+offerDiscount(5) ? 'yes' : 'no');
+
+///*** Typescript Types ***
+console.log('-=====TYPESCRIPT TYPES=====-');
+///* Any type - should be avoided **********
+console.log('--ANY TYPE--');
+let price;
+price = 25;
+price = 'price';
+
+///* Void type **********
+console.log('--VOID TYPE--');
+let selectedOrder;
+function selectOrder(order: string): void {
+  selectedOrder = order;
+}
+
+console.log('Selected order: ' + selectOrder('salad'));
+
+///* Union type **********
+console.log('--UNION TYPE--');
+let selectedGender: string = 'men';
+
+function selectGender(gender: 'men' | 'women' | 'unisex'): void {
+  //also works with numbers and booleans
+  selectedGender = gender;
+}
+selectGender('men');
+
+console.log('You have chosen the gender: ' + selectedGender);
+
+///* Function type **********
+console.log('--FUNCTION TYPE--');
+
+let totalPrice: (price: number, quantity?: number) => number; //type definition with the return statement
+//alternatively
+//let sumItems:(price:number, quantity:number)=>number=(x,y)=>x*y;
+
+totalPrice = (price: number, quantity?: number): number => {
+  if (quantity) {
+    return quantity * price;
+  }
+  return price;
+};
+
+console.log('Total price : ' + totalPrice(12, 5));
+
+//alternatively
+//sumItems = (x,y) => x*y;
